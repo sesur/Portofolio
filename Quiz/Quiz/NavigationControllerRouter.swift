39a0953e2bc29a139e4b7aa197f11d7d4320e5b1
Sergiu <sergiusuru@Sergius-MacBook-Pro.local>
2020-04-04 16:27:24 +0300

    Decoupling modules, creating abstract factories for routing

--- Quiz/Quiz/NavigationControllerRouter.swift
+++ Quiz/Quiz/NavigationControllerRouter.swift
@@ -9,16 +9,26 @@
 import UIKit
 import QuizEngine
 
+
+
+protocol ViewControllerFactory {
+    func questionViewController(for question: String, answerCallback: @escaping (String) -> Void) -> UIViewController
+}
+
+
 class NavigationControllerRouter: Router {
     private let navigationController: UINavigationController
+    private let factory: ViewControllerFactory
     
-    init(_ navigationController: UINavigationController) {
+    init(_ navigationController: UINavigationController, factory: ViewControllerFactory) {
         self.navigationController = navigationController
+        self.factory = factory
     }
     
     
     func routeTo(question: String, answerCallback: @escaping (String) -> Void) {
-        navigationController.pushViewController(UIViewController(), animated: false)
+        let viewController = factory.questionViewController(for: question, answerCallback: answerCallback)
+        navigationController.pushViewController(viewController, animated: false)
     }
     
     func routeTo(result: Results<String, String>) {
--- Quiz/QuizTests/NavigationControllerRouterTests.swift
+++ Quiz/QuizTests/NavigationControllerRouterTests.swift
@@ -13,21 +13,86 @@ import QuizEngine
 
 class NavigationControllerRouterTests: XCTestCase {
     
-    func test_routeToQuestion_presentQuestionController() {
+    //    func test_routeToQuestion_presentQuestionController() {
+    //        let navigationController = UINavigationController()
+    //        let sut = NavigationControllerRouter(navigationController)
+    //
+    //        sut.routeTo(question: "Q1", answerCallback: { _ in })
+    //        XCTAssertEqual(navigationController.viewControllers.count, 1)
+    //    }
+    //
+    //    func test_routeToQuestionTwice_presentQuestionController() {
+    //        let navigationController = UINavigationController()
+    //        let sut = NavigationControllerRouter(navigationController)
+    //
+    //        sut.routeTo(question: "Q1", answerCallback: { _ in })
+    //        sut.routeTo(question: "Q2", answerCallback: { _ in })
+    //
+    //        XCTAssertEqual(navigationController.viewControllers.count, 2)
+    //    }
+    
+    func test_routeToQuestion_showsViewController() {
         let navigationController = UINavigationController()
-        let sut = NavigationControllerRouter(navigationController)
+        let factory = ViewControllerFacoryStub()
+        let viewController = UIViewController()
+        factory.stub(question: "Q1", with: viewController)
+        
+        let sut = NavigationControllerRouter(navigationController, factory: factory)
         
         sut.routeTo(question: "Q1", answerCallback: { _ in })
+        
         XCTAssertEqual(navigationController.viewControllers.count, 1)
+        XCTAssertEqual(navigationController.viewControllers.first, viewController)
     }
     
-    func test_routeToQuestionTwice_presentQuestionController() {
+    
+    func test_routeToSecondQuestion_showsViewController() {
         let navigationController = UINavigationController()
-        let sut = NavigationControllerRouter(navigationController)
+        let factory = ViewControllerFacoryStub()
+        let viewController = UIViewController()
+        let secondViewController = UIViewController()
+        factory.stub(question: "Q1", with: viewController)
+        factory.stub(question: "Q2", with: secondViewController)
+        
+        let sut = NavigationControllerRouter(navigationController, factory: factory)
         
         sut.routeTo(question: "Q1", answerCallback: { _ in })
         sut.routeTo(question: "Q2", answerCallback: { _ in })
         
         XCTAssertEqual(navigationController.viewControllers.count, 2)
+        XCTAssertEqual(navigationController.viewControllers.first, viewController)
+        XCTAssertEqual(navigationController.viewControllers.last, secondViewController)
+    }
+    
+    func test_routeToSecondQuestion_showsViewController_withRightCallback() {
+        let navigationController = UINavigationController()
+        let factory = ViewControllerFacoryStub()
+        let viewController = UIViewController()
+        factory.stub(question: "Q1", with: viewController)
+        
+        let sut = NavigationControllerRouter(navigationController, factory: factory)
+        
+        var answerCallBackFired = false
+        sut.routeTo(question: "Q1", answerCallback: { _ in answerCallBackFired = true })
+        factory.answerCallback["Q1"]!("anything")
+        
+        XCTAssertTrue(answerCallBackFired)
+    }
+    
+    
+    
+    class ViewControllerFacoryStub: ViewControllerFactory {
+        
+        private var stubbedQuestions = [String: UIViewController]()
+        var answerCallback = [String: (String) -> Void]()
+        
+        func stub(question: String, with viewController: UIViewController) {
+            stubbedQuestions[question] = viewController
+        }
+        
+        func questionViewController(for question: String, answerCallback: @escaping (String) -> Void) -> UIViewController {
+            self.answerCallback[question] = answerCallback
+            return stubbedQuestions[question]!
+        }
     }
 }
