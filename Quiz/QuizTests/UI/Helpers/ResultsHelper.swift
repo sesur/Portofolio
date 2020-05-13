7e6132477670bd3d9adefde08eb35a6bc76939c2
Sergiu <sergiusuru@Sergius-MacBook-Pro.local>
2020-05-14 01:58:36 +0300

    Refactoring Results tests helpers

--- Quiz/QuizTests/UI/Helpers/ResultsHelper.swift
+++ Quiz/QuizTests/UI/Helpers/ResultsHelper.swift
@@ -9,15 +9,21 @@
 import Foundation
 @testable import QuizEngine
 
-extension Results: Hashable {
+extension Results {
     static func make(answers: [Question: Answer] = [:], score: Int = 0 ) -> Results {
         return Results(answers: answers, score: score)
     }
-    
+}
+
+extension Results: Equatable where Answer: Equatable {
+    public static func ==(lhs: Results<Question, Answer>, rhs: Results<Question, Answer>) -> Bool {
+        return lhs.score == rhs.score && lhs.answers == rhs.answers
+    }
+}
+
+
+extension Results: Hashable where Answer: Equatable {
     public func hash(into hasher: inout Hasher) {
         hasher.combine(1)
-    }
-    public static func ==(lhs: Results<Question, Answer>, rhs: Results<Question, Answer>) -> Bool {
-        return lhs.score == rhs.score
     }
 }
