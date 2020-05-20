3bb00b33d235c6016dd08e197da6a80e1ab5c592
Sergiu <sergiusuru@Sergius-MacBook-Pro.local>
2020-05-20 22:41:12 +0300

    Update UI & API link for all vertions

--- Star Trivia/Star Trivia v0.1/Star Trivia/Utilities/Constants.swift
+++ Star Trivia/Star Trivia v0.1/Star Trivia/Utilities/Constants.swift
@@ -11,5 +11,5 @@ import UIKit
 let BLACK_BACKGROND = UIColor.black.withAlphaComponent(0.4).cgColor
 
 
-let BASE_URL = "https://swapi.co/api/"
+let BASE_URL = "https://swapi.dev/api/"
 let PERSON_URL = BASE_URL + "people/"
